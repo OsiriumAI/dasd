b779eb8d65b73925bfeae8a0b0ad14221a9d0fca
Osirium_Labs <142729653+OsiriumAI@users.noreply.github.com>
2025-06-12 10:06:10 +0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -17,11 +17,17 @@ const config = {
         binance: { command: 'npx', args: ['binance-mcp'] },
         coinstats: {
           command: "npx",
-          args: ["-y", "@coinstats/coinstats-mcp"],
+          args: ["@coinstats/coinstats-mcp"],
           env: {
             COINSTATS_API_KEY: "3e/2YQNeNkHl5QOy+v+uMUgSR4KPIc/wMZ1QSvey+jA="
           }
-        }
+        },
+        alchemy: {
+      command: "npx",
+      args: [ "@alchemy/mcp-server"],
+      env: {ALCHEMY_API_KEY: "OxoJnMMRJXnlTrcqNkv0gPfZwBm6qU2v"}
+       },
+       
     // Add more servers here as needed
   }
 };
